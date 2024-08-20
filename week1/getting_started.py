375f2a84ef5dcfdf1856387c2ab26bdc84584f19
Sami Rollins <srollins@gmail.com>
2024-08-19 17:56:28 -0700

    update examples

---
+++ week1/getting_started.py
@@ -0,0 +1,16 @@
+"""
+A program to practice with some of the features of python.
+"""
+
+def main():
+    pass
+    
+    # print a greeting
+
+    # store the greeting in a variable and print the greeting
+
+    # ask the the user's name and greet that user
+
+
+if __name__ == '__main__':
+    main()
