751b4e44be47aec20a457f4bd2493ba1fcde7bd8
Sami Rollins <srollins@gmail.com>
2024-09-13 13:22:06 -0700

    update week 3 examples

--- week3/loops.py
+++ week3/loops.py
@@ -1,14 +1,3 @@
-"""
-Write a program (remember that programs have a main() function) to print the lyrics of the song "Old MacDonalds." Your program should print the lyrics for five different animals (five verses), similar to the example verse below:
-"""
-
-# Old MacDonald had a farm, ee-igh, ee-igh, oh!
-# And on that farm he had a cow, ee-igh, ee-igh, oh!
-# With a moo, moo here and a moo, moo there.
-# Here a moo, there a moo, everywhere a moo, moo.
-# Old MacDonald had a farm, ee-igh, ee-igh, oh!
-
-
 def login():
     """Implement a function that prompts a 
 	user for a password until the actual 
