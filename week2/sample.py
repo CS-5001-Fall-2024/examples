2dc5277b83dae1e17bd4da9e773a23ea525074ad
Sami Rollins <srollins@gmail.com>
2024-09-13 13:14:08 -0700

    add week 3 code

--- week2/sample.py
+++ week2/sample.py
@@ -1,14 +1,30 @@
 
 
-def main(): 
-    print('hello, world')
 
-x = 3
-y = 9
-sum = x + y
-print(f'your sum is: {sum}')
 
-main()
-main()
+def calculate(number):
+    print("LLLK")
+    return number / 60    
 
-#if __name__ == '__main__':
+def mystery(number):
+    result = calculate(number)
+    return result
+
+result = mystery(90)
+print(result)
+print(mystery(60))
+
+
+
+# def main(): 
+#     print('hello, world')
+
+# x = 3
+# y = 9
+# sum = x + y
+# print(f'your sum is: {sum}')
+
+# main()
+# main()
+
+# #if __name__ == '__main__':
---
+++ week3/loops.py
@@ -0,0 +1,28 @@
+"""
+Write a program (remember that programs have a main() function) to print the lyrics of the song "Old MacDonalds." Your program should print the lyrics for five different animals (five verses), similar to the example verse below:
+"""
+
+# Old MacDonald had a farm, ee-igh, ee-igh, oh!
+# And on that farm he had a cow, ee-igh, ee-igh, oh!
+# With a moo, moo here and a moo, moo there.
+# Here a moo, there a moo, everywhere a moo, moo.
+# Old MacDonald had a farm, ee-igh, ee-igh, oh!
+
+
+def login():
+    """Implement a function that prompts a 
+	user for a password until the actual 
+	password is entered and the user is 
+	authenticated.
+
+	Step 2: allow a maximum of three attempts.
+
+	"""
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
