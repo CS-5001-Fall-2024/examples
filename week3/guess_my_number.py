79323ee61d24919f3c91ee06d68dc8cdd5ca13d2
Sami Rollins <srollins@gmail.com>
2024-09-19 16:01:54 -0700

    update guess my number

--- week3/guess_my_number.py
+++ week3/guess_my_number.py
@@ -14,8 +14,47 @@ Phase 3:
 number of guesses too low
 """
 
+import random
+LOW = 1
+HIGH = 10
+
+def generate_secret_number() -> int:
+    number = random.randint(LOW, HIGH)
+    return number
+
+def get_user_guess() -> int:
+    user_guess = int(input('What is your guess? '))
+    return user_guess
+
+def compare(secret_number: int, user_guess: int) -> bool:
+    """Return True if secret_number matches user_guess
+    and False otherwise.
+    """
+    return secret_number == user_guess
+
+def display_result(result: bool):
+    if result:
+        print('You guessed correctly!\nYou win!')
+    else:
+        print('Try again...')
+
+def play():
+    secret_number = generate_secret_number()
+    # cheat for testing    
+    print(f'secret number is {secret_number}')
+    
+    user_guess = get_user_guess()
+    result = compare(secret_number, user_guess)
+    # while result == False:
+    # while result != True:
+    while not result: 
+        display_result(result)
+        user_guess = get_user_guess()
+        result = compare(secret_number, user_guess)
+    display_result(result)
+
 def main():
-    pass
+    play()
 
 if __name__ == '__main__':
     main()
