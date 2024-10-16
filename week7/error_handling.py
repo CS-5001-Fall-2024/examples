ec51066c2e4b326921d1136676bc209367fb510c
Sami Rollins <srollins@gmail.com>
2024-10-16 11:40:09 -0700

    update error handling examples

--- week7/error_handling.py
+++ week7/error_handling.py
@@ -11,20 +11,32 @@ def convert(value, target_type):
     If the target_type is not int or float
         generate a TypeError
     If the value cannot be converted
-        return a ValueError
+        generate a ValueError
     """
     # '5'
     # int
-    pass
+    if isinstance(value, target_type):
+        return value
     
+    if target_type != int and target_type != float:
+        raise TypeError('You gave me an invalid type!')
+    
+    if target_type == int:
+        new_value = int(value)
+    elif target_type == float:
+        new_value = float(value)
+    return new_value
 
+    
 def read_file(filename: str):
     """
     Write a function to display the contents of a file.
     The function will throw an exception if the file is
     not found.
     """
-    pass
+    with open(filename, 'r') as input_file:
+        for line in input_file:
+            print(line)
 
 def search_replace(search:str, 
     replace:str, 
@@ -56,9 +68,47 @@ def find_py_files(root: str):
     '''
     pass
 
-def main():
-    pass
 
+def pass_by_reference(local_list: list[str]):
+    local_list.append('XXX')
+
+
+def main():
+
+    read_file('error_handling.py')
+
+    # to_convert = 4
+    # print(f'Type of to_convert = {type(to_convert)}')
+    # try:
+    #     value = convert(to_convert, str)
+    #     print(f'Type of converted value = {type(value)}')
+    # except ValueError as ve:
+    #     print('invalid conversion attempted')
+    #     print(ve)
+    # except TypeError as te:
+    #     print('invalid type provided')
+
+    # print('still going...')
+    # value = convert('3.5', float)
+
+    # list = ['a', 'b', 'c']
+    # print(list)
+    # pass_by_reference(list)
+    # print(list)
+
+    # valid_number = False
+    # while not valid_number:
+    #     number = input('Give me a number: ')
+    #     try:
+    #         float_number = float(number)
+    #         valid_number = True
+    #     except ValueError as ve:
+    #         print('number is invalid')
+    #         print(ve)
+    #         # number = input('Give me a number: ')
+            
+    # print(float_number)
+    # print(type(float_number))
 
 if __name__ == '__main__':
     main()
